ad8e83b135c1a1e21bcde04cc7c7a8e2cbba387d
unknown <muhammadyarkhan31@gmail.com>
2026-03-02 23:20:52 +0500

    add .shape check

--- practice_tenserflow.py
+++ practice_tenserflow.py
@@ -1,22 +1,20 @@
 import tensorflow as tf
 import time
 
-#looking for availabel gpus
-gpus = tf.config.list_physical_devices('GPU')
-print("GPUs available:", gpus)
 
-# v1 = tf.constant(5)
-# v2 = tf.Variable(10)
-#
-# print(v1)
-# print(v2)
-#
-# #checking mutability of the Variables
-#v3 = tf.Variable(3)
-# print("Before", v3)
-#
-# v3.assign(4)
-# print("After", v3)
+
+v1 = tf.constant(5)
+v2 = tf.Variable(10)
+
+print(v1)
+print(v2)
+
+#checking mutability of the Variables
+v3 = tf.Variable(3)
+print("Before", v3)
+
+v3.assign(4)
+print("After", v3)
 
 #Checking how much time is taken by tensorflow for this single variable
 # start = time.time()
@@ -44,11 +42,13 @@ print("Multiplication:", e)
 f = tf.divide(a, b)
 print("Division:", f)
 
-#multidimensional tensors
+# #multidimensional tensors
 t1 = tf.constant([[1,2,3],[4,5,6]])
 t2 = tf.constant([[7,6,8],[1,5,7]])
-
+#
 two_ts_add = tf.add(t1,t2)
-print("Two TS Add:", two_ts_add)
+# print("Two TS Add:", two_ts_add)
+#
+# print("Shape of t1",t1.shape)
 
-print("Shape of t1",t1.shape)
+print("",two_ts_add.shape)
